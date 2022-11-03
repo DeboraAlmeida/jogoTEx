bea7d278a8b6a9221909cbe04df94f505652d851
Debora Almeida <deboraalmeida89@gmail.com>
2022-11-03 20:18:40 -0300

    Added info user recuperável no localStorage e css básico da tela de jogo

--- src/js/script.js
+++ src/js/script.js
@@ -4,9 +4,10 @@ const pageGame  = document.querySelector('#telaJogo')
 const campo = document.querySelector('#nome');
 
 btn.addEventListener("click", () => {
-    alert(campo.value)
+    localStorage.setItem('user', campo.value)
     page.setAttribute("class", "hide")
-    pageGame.classList.add("show")    
+    pageGame.classList.add("show")  
+    document.querySelector('#user').innerText = localStorage.getItem('user', campo.value)  
 })
 // OU
 /* btn.onclick = () => {
